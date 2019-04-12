ebc36a574dbd511bf271b6856b38d4ae066cb96c
unknown <toan.phan2501workplace@gmail.com>
2019-04-12 07:43:45 +0700

    first commit

---
+++ lib/utils/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/image_04.jpg",
+  "assets/image_03.jpg",
+  "assets/image_02.jpg",
+  "assets/image1.jpg",
+];
+
+List<String> title = [
+  "Hounted Ground",
+  "Fallen In Love",
+  "The Dreaming Moon",
+  "Xem doanh thu của bạn",
+];
